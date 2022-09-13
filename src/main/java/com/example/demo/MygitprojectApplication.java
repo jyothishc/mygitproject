ea89d8de3115048f6690435beebacd40efcf5734
jyotheesh k <jyothishc30@gmail.com>
2022-09-13 11:08:33 +0530

    new project creating

---
+++ src/main/java/com/example/demo/MygitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygitprojectApplication.class, args);
+	}
+
+}
